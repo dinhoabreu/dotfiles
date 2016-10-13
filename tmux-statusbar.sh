74a4752237f1abf523a9c5495c72690051da9af3
Edison E. Abreu <dinho.abreu@gmail.com>
2016-10-13 12:12:34 -0300

    Add current itunes music on tmux statusbar

---
+++ tmux-statusbar.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+tmux_get() {
+	tmux show -v -g "$1"
+}
+
+cmd_exists() {
+	hash "$1" 2>/dev/null
+}
+
+main() {
+	
+	local status_right
+
+	cd "$( dirname "${BASH_SOURCE[0]}" )" || return 1
+	
+	if cmd_exists osascript; then
+		status_right="$(tmux_get status-right)"
+		tmux set -g status-right "#[fg=colour42]#(osascript $PWD/applescripts/tunes.js) $status_right"
+	fi
+}
+
+main
