25c5a7b892a78e0ed79450b28089c489350af465
Edison E. Abreu <dinho.abreu@gmail.com>
2018-01-31 22:46:09 -0200

    Tmux statusbar inbox and itunes optional

--- tmux-statusbar.sh
+++ tmux-statusbar.sh
@@ -8,15 +8,31 @@ cmd_exists() {
 	hash "$1" 2>/dev/null
 }
 
+join_by() {
+
+	local d=$1
+
+	shift
+	echo -n "$1"
+	shift
+	printf '%s' "${@/#/$d}"
+}
+
 main() {
-	
-	local status_right
+
+	local status_right info=()
 
 	cd "$( dirname "${BASH_SOURCE[0]}" )" || return 1
-	
+
 	if cmd_exists osascript; then
 		status_right="$(tmux_get status-right)"
-    tmux set -g status-right "#[fg=colour42]#(osascript $PWD/applescripts/inbox.js) • #(osascript $PWD/applescripts/tunes.js) $status_right"
+		if [[ $TMUX_STATUSBAR_INBOX -eq 1 ]]; then
+			info+=("#[fg=colour42]#(osascript $PWD/applescripts/inbox.js)")
+		fi
+		if [[ $TMUX_STATUSBAR_TUNES -eq 1 ]]; then
+			info+=("#(osascript $PWD/applescripts/tunes.js) ")
+		fi
+		tmux set -g status-right "$(join_by ' • ' "${info[@]}") $status_right"
 	fi
 }
 
